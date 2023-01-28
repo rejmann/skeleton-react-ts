4ef876fce2bc576ef6066afe88403011ba3a7562
Rejman <rejmans00@gmail.com>
2023-01-28 15:43:56 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## React TS com Docker
+
+Ambiente de desenvolvimento frontend react-ts com docker
+
+## Tecnologias
+
+- React com typescript
+- Tailwind Css
+- Docker
+
+## Como usar
+
+Temos no diretório ```./cli``` o script bash ```node```, e com ele poderá ser executado qualquer comando relacionado ao node.
+
+Execute ```./cli/node npm install``` para instalar as dependências.
+
+E para subir o ambiente ```docker compose up```
---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+export default function App() {
+  return (
+    <div className="App">
+      <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+        <div className="mx-auto max-w-md">
+          <div className="space-y-6 py-8 text-base leading-7 text-gray-600">
+            <ul className="space-y-4">
+              <li className="flex items-center">
+                <svg className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" stroke-linecap="round" stroke-linejoin="round">
+                  <circle cx="12" cy="12" r="11" />
+                  <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
+                </svg>
+                <p className="ml-4">Skeleton React-ts com Docker</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
